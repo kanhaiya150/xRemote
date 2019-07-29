8813c4bb4e77495bb8d8c7e9cac0f63d0ee56aba
Kanhaiya Lal <50043912+kanhaiya150@users.noreply.github.com>
2019-07-29 20:28:07 +0100

    Add files via upload
    manual answer script which ask password

---
+++ interact.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#set -xv
+
+echo "Hello, How are you?"
+
+read $REPLY
+
+echo "May I ask you a question?"
+
+read $REPLY
+
+echo "What is your password?"
+
+read $REPLY
+
+echo "What is your role in Wipro?"
+
+read $REPLY
+
+echo "Cool, how much experience you have?"
+
+read $REPLY
